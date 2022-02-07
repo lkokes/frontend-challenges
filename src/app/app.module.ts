8bae97dc22254104bb4c970638a1a0f47032e9ab
Lucia Kokesova <lkokesova@gmail.com>
2022-02-07 12:31:49 +0100

    Organize assets folder and change paths to images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
+import {MatChipsModule} from '@angular/material/chips';
+
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { QrCodeComponent } from './qr-code/qr-code.component';
 import { SocialProofSectionComponent } from './social-proof-section/social-proof-section.component';
 import { PreviewCardComponent } from './preview-card/preview-card.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatChipsModule} from '@angular/material/chips';
 
 @NgModule({
   declarations: [
